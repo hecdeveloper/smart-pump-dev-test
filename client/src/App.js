ee5f0ff4ffe8dd14b3a2afb06583f444bffee3bc
Hector <hector.dev.ui@gmail.com>
2024-12-12 16:55:08 -0600

    private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import EditProfile from './components/EditProfile';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/edit-profile" element={<EditProfile/>}/>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={
+            <PrivateRoute>
+              <EditProfile />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
